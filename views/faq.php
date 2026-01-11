9f2c5bc2358e62ddd1f7ee2a2fcf11a78c103f30
Hamim Bhuiyan <hamimbhuiyan@Hamims-MacBook-Air.local>
2026-01-12 00:05:54 +0600

    Created HeroPage with logo

---
+++ views/faq.php
@@ -0,0 +1,14 @@
+<?php 
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head><title>Services</title>
+</head>
+<body>
+    
+</body>
+
+</html>
---
+++ views/landing.php
@@ -0,0 +1,38 @@
+<?php 
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Polok Online</title>
+        <link rel="stylesheet" href=css/stylesheet.css >
+    
+    </head>
+    <body>
+         <header>
+        <div class="logo">
+            <img src="css/logo.jpeg" alt="Logo">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="services.php">Services</a></li>
+                <li><a href="coverage.php">Coverage Area</a></li>
+                <li><a href="offers.php">Offers & Promotion</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="about.php">About Us</a></li>
+                <li><a href="faq.php">FAQ</a></li>
+            </ul>
+        </nav>
+        <button class="signin-btn">SIGNIN / LOGIN</button>
+    </header>
+    
+    <section class="hero">
+        <h1>Welcome to Our Services</h1>
+        <p>Providing the best solutions for your business</p>
+        <button class="hero-btn">Explore our packages</button>
+    </section>
+    </body>
+    </html>
+
