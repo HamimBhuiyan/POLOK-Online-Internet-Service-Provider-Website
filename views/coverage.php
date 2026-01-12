30a3438a9113d30361c4e9544a37a21ad3db87b1
Hamim Bhuiyan <hamimbhuiyan@Hamims-MacBook-Air.local>
2026-01-12 07:45:39 +0600

    Feature Section Added

--- views/coverage.php
+++ views/coverage.php
@@ -5,7 +5,7 @@
 
 <!DOCTYPE html>
 <html>
-    <head><title>Services</title>
+    <head><title>Coverage</title>
 </head>
 <body>
     
--- views/landing.php
+++ views/landing.php
@@ -7,7 +7,7 @@
 <html>
     <head>
         <title>Polok Online</title>
-        <link rel="stylesheet" href=css/stylesheet.css >
+        <link rel="stylesheet" href="css/stylesheet.css" >
     
     </head>
     <body>
@@ -29,10 +29,60 @@
     </header>
     
     <section class="hero">
-        <h1>Welcome to Our Services</h1>
-        <p>Providing the best solutions for your business</p>
+        <h1>Welcome to Polok Online Internet Service</h1>
+        <p>Providing the best packages for your home & business </p>
         <button class="hero-btn">Explore our packages</button>
     </section>
+
+    <section class="feature-section">
+    <h1 class="section-title">Services & Solutions</h1>
+    <p class="section-subtitle">Join today to take advantage of Polok Online's great features </p>
+
+    <div class="box-container">
+        <div class="box">
+            <h3>Broadband Internet</h3>
+            <p>High-speed internet for home and office</p>
+        </div>
+
+        <div class="box">
+            <h3>Fiber Optic Connections</h3>
+            <p>Reliable for streaming, gaming, and business</p>
+        </div>
+        
+        <div class="box">
+            <h3>Secure Payments</h3>
+            <p>Safe and trusted</p>
+        </div>
+        <div class="box">
+            <h3>Affordable Prices</h3>
+            <p>Best value guaranteed</p>
+        </div>
+
+        <div class="box">
+            <h3>Custom Packages</h3>
+            <p>Flexible solutions</p>
+        </div>
+
+        <div class="box">
+            <h3>Business Internet Plans</h3>
+            <p>Dedicated internet for offices</p>
+        </div>
+
+
+        <div class="box">
+            <h3>24/7 Support</h3>
+            <p>Always available for you</p>
+        </div>
+
+
+        <div class="box">
+            <h3>Network Security Solutions</h3>
+            <p>Secure home networks</p>
+        </div>
+
+    </div>
+</section>
+
     </body>
     </html>
 
