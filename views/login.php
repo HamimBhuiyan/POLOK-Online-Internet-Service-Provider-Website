adfe6773c1eeac73bc4783979ca220b8b0a517c0
Hamim Bhuiyan <hamimbhuiyan@Hamims-MacBook-Air.local>
2026-01-16 00:59:35 +0600

    Login Page

--- views/login.php
+++ views/login.php
@@ -2,3 +2,41 @@
 
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+    
+        <div class="login-box">
+
+            <div class="logo">
+                <img src="css/logo.jpeg" alt="Logo">
+            </div>
+
+            <h2>Login</h2>
+
+            <form>
+                <input type="email" placeholder="Email" required>
+                <input type="password" placeholder="Password" required>
+
+                <div class="options">
+                    <label>
+                        <input type="checkbox"> Remember me
+                    </label>
+                    <a href="">Forgot password?</a>
+                </div>
+
+                <button type="submit">Login</button>
+            </form>
+
+            <p class="signup">
+                Don't have an account? <a href="">Sign up now</a>
+            </p>
+
+        </div>
+</body>
+</html>
